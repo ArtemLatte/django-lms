2a2d261ba35d8b0c63fc957576b2954b56d0459f
Artem <usmad42@mail.ru>
2026-03-10 19:50:25 +0700

    Add djangorestframework and update requirements

--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import TeacherSerializer
+from . import models
+from rest_framework.response import Response
+from rest_framework import generics
 
-# Create your views here.
+class TeacherList(generics.ListCreateAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
