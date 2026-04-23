5542d1447f61939cd5d72b5d93c7530360ede7b1
Ignatiy <uolax@Ignatiys-MacBook-Air.local>
2026-04-23 12:39:46 +0700

    TeacherLogin and TeacherLogout

--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from .serializers import TeacherSerializer
 from . import models
 from rest_framework.response import Response
@@ -15,3 +17,13 @@ class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+@csrf_exempt
+def teacher_login(request):
+    email=request.POST['email']
+    password=request.POST['password']
+    teacherData=models.Teacher.objects.get(email=email,password=password)
+    if teacherData:
+        return JsonResponse({'bool':True})
+    else:
+        return JsonResponse({'bool':False})
