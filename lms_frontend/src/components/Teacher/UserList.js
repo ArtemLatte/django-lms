7f3dc63673961a63f265dff5520caed5c1e29b68
Ignatiy <uolax@Ignatiys-MacBook-Air.local>
2026-04-29 13:48:57 +0700

    Show All Enrolled student list

--- lms_frontend/src/components/Teacher/UserList.js
+++ lms_frontend/src/components/Teacher/UserList.js
@@ -1,6 +1,25 @@
-import {Link} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import TeacherSidebar from './TeacherSidebar';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+const baseUrl = 'http://127.0.0.1:8000/api'
 function UserList() {
+    const [StudentData,setStudentData]=useState([]);
+
+    const teacherId=localStorage.getItem('teacherId');
+    //Fetch courses when page load
+    useEffect(()=> {
+        try{
+        axios.get(baseUrl+'/fetch-all-enrolled-students/' + teacherId)
+        .then((res)=>{
+                console.log(res.data)
+                setStudentData(res.data);
+        });
+        }catch(error){
+            console.log(error);
+        }
+    },[]);
+
     return (
         <div className="container mt-4">
             <div className="row">
@@ -9,22 +28,28 @@ function UserList() {
                 </aside>
                 <section className="col-md-9">
                     <div className="card">
-                        <h5 className="card-header">My Users</h5>
+                        <h5 className="card-header">All Student List</h5>
                         <div className="card-body">
                             <table className="table table-bordered">
                                 <thead>
                                     <tr>
                                         <th>Name</th>
-                                        <th>Enrolled Course</th>
-                                        <th>Action</th>
+                                        <th>Email</th>
+                                        <th>Username</th>
+                                        <th>Interested Categories</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <td><Link to="/">John Doe</Link></td>
-                                    <td><Link to="/">Php</Link></td>
-                                    <td>
-                                        <button className="btn btn-danger btn-sm active">Delete</button>
-                                    </td>
+                                    {StudentData.map((row,index) =>
+                                    <tr>
+                                        <td>{row.student.full_name}</td>
+                                        <td>{row.student.email}</td>
+                                        <td>{row.student.username}</td>
+                                        <td>
+                                            {row.student.interested_categories}
+                                        </td>
+                                    </tr>
+                                    )}
                                 </tbody>
                             </table>
                         </div>
